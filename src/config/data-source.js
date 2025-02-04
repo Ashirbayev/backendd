74321971885987fc7ad6502a9ab449e61fc91830
Yerlan <E.Ashirbayev@gmail.com>
2025-02-04 09:01:23 +0500

    process.env.PORT

--- src/config/data-source.js
+++ src/config/data-source.js
@@ -2,7 +2,7 @@ const { DataSource } = require("typeorm");
 
 const AppDataSource = new DataSource({
     type: "postgres",
-    host: process.env.DB_HOST || 'postgresql://mydb_02ar_user:JhDlY68ypy7TSrcmBPItklX1Q2J17rcJ@dpg-cugomhjtq21c73f1qs10-a.oregon-postgres.render.com/mydb_02ar',
+    host: process.env.DB_HOST || 'postgresql://mydb_02ar_user:JhDlY68ypy7TSrcmBPItklX1Q2J17rcJ@dpg-cugomhjtq21c73f1qs10-a/mydb_02ar',
     port: Number(process.env.DB_PORT) || 5432,
     username: process.env.DB_USERNAME || 'mydb_02ar_user',
     password: process.env.DB_PASSWORD || 'JhDlY68ypy7TSrcmBPItklX1Q2J17rcJ',
