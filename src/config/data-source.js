b1b5fef12c85747842a3cc54461898102d0a10d1
Yerlan <E.Ashirbayev@gmail.com>
2025-02-04 09:04:26 +0500

    env host

--- src/config/data-source.js
+++ src/config/data-source.js
@@ -2,11 +2,11 @@ const { DataSource } = require("typeorm");
 
 const AppDataSource = new DataSource({
     type: "postgres",
-    host: process.env.DB_HOST || 'postgresql://mydb_02ar_user:JhDlY68ypy7TSrcmBPItklX1Q2J17rcJ@dpg-cugomhjtq21c73f1qs10-a/mydb_02ar',
-    port: Number(process.env.DB_PORT) || 5432,
-    username: process.env.DB_USERNAME || 'mydb_02ar_user',
-    password: process.env.DB_PASSWORD || 'JhDlY68ypy7TSrcmBPItklX1Q2J17rcJ',
-    database: process.env.DB_NAME || 'mydb_02ar',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     entities: [
         require("../models/User"),
         require("../models/Group"),
